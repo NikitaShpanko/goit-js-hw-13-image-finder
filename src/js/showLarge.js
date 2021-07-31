6ded23befcf7d0f457f3c78e3841dd6858b21a8f
Nikita Shpanko <shpancore@gmail.com>
2021-07-31 20:34:57 +0300

    added escape through clicking outside modal on mobile

--- src/js/showLarge.js
+++ src/js/showLarge.js
@@ -3,11 +3,27 @@ import modalTpl from '../tpl/modal.hbs';
 
 export default function (selector, escapeKeys) {
   let instance;
+
   selector.addEventListener('click', e => {
+    // FOR MOBILE://
+    if (basicLightbox.visible()) {
+      instance.close();
+      e.preventDefault();
+      return;
+    }
+    ////////////////
     if (e.target.tagName !== 'IMG') return;
     e.preventDefault();
     instance = basicLightbox.create(modalTpl(e.target.dataset));
     instance.show();
+
+    // console.log(instance.element().getBoundingClientRect());
+    // instance
+    //   .element()
+    //   .querySelector('img')
+    //   .addEventListener('load', e => {
+    //     console.log(e.target.getBoundingClientRect());
+    //   });
   });
 
   document.addEventListener('keydown', e => {
