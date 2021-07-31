cba864ca0c767d1b87e46368b66debd28acebad8
Nikita Shpanko <shpancore@gmail.com>
2021-07-31 03:24:30 +0300

    implemented vanilla form submit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,13 @@ const html = new QuerySel('body', 'form', 'input', '.gallery');
 html.body.style.paddingTop = html.form.clientHeight + 'px';
 const pixabayGallery = new PixabayGallery(html.gallery, config);
 
-html.form.addEventListener('submit', e => {
-  e.preventDefault();
-  pixabayGallery.updateQuery(html.input.value);
-});
+// html.form.addEventListener('submit', e => {
+//   //e.preventDefault();
+//   //pixabayGallery.updateQuery(html.input.value);
+// });
+
+const startupQuery = new URL(location).searchParams.get('query');
+if (startupQuery) {
+  html.input.value = startupQuery;
+  pixabayGallery.updateQuery(startupQuery);
+}
